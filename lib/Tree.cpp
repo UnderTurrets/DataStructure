c1ece6adf4081bd4e30b8a5c0b8923d5f1369450
Xu Han <736946693@qq.com>
2023-01-08 00:16:16 -0800

    20230108 16:16

--- lib/Tree.cpp
+++ lib/Tree.cpp
@@ -21,7 +21,7 @@ template<typename T>
 BinTree<T>::BinTree(vector<T> nums){
     this->val=sortedArrayToBST_helper(nums,0,nums.size()-1)->val;
     this->left=sortedArrayToBST_helper(nums,0,nums.size()-1)->left;
-    this->left=sortedArrayToBST_helper(nums,0,nums.size()-1)->right;
+    this->right=sortedArrayToBST_helper(nums,0,nums.size()-1)->right;
 }
 template<typename T>
 BinTree<T>* BinTree<T>::sortedArrayToBST_helper(vector<T>& nums, int left, int right) {
@@ -194,7 +194,6 @@ BinTree<T>::BinTree(vector<T> preorder, vector<T> inorder) {
         BinTree<T> *temp;
         temp=this;
         queue<BinTree<T>*> Q;
-        if(!temp)return;
         Q.push(temp);
         while (!Q.empty()){
             temp=Q.front();
@@ -211,10 +210,9 @@ BinTree<T>::BinTree(vector<T> preorder, vector<T> inorder) {
 
     //按层形成二维数组（层序遍历的应用）
     template<typename T>
-    vector<vector<int>> BinTree<T>::levelOrder() {
-        vector<vector<int>>ret;
+    vector<vector<T>> BinTree<T>::levelOrder() {
+        vector<vector<T>>ret;
         queue<BinTree<T>*> Q;
-        if(!this)return {{}};
         Q.push(this);
         while (!Q.empty()){
             int x=Q.size();
@@ -229,12 +227,11 @@ BinTree<T>::BinTree(vector<T> preorder, vector<T> inorder) {
         return ret;
     }
 
-    //给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+    //给你二叉树的根节点 this ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
     template<typename T>
-    vector<vector<int>> BinTree<T>::zigzagLevelOrder(){
-        vector<vector<int>>ret;
+    vector<vector<T>> BinTree<T>::zigzagLevelOrder(){
+        vector<vector<T>>ret;
         queue<BinTree<T>*> Q;
-        if(!this)return {{}};
         Q.push(this);
         int flag=0;
         while (!Q.empty()){
@@ -254,44 +251,54 @@ BinTree<T>::BinTree(vector<T> preorder, vector<T> inorder) {
 
     //先序遍历递归，以所给参数为根节点开始遍历
     template<typename T>
-    void BinTree<T>::Travesal_preorder_recurrence (BinTree<T> *myself){
-        if(myself){
+    void BinTree<T>::Travesal_preorder_recurrence_helper (BinTree<T> *root){
+        if(root){
             //在此进行你要执行的操作
-            cout<<myself->val<<endl;
+            cout<<root->val<<endl;
 
 
 
 
             //在此以上进行你要执行的操作
-            Travesal_preorder_recurrence(myself->left);
-            Travesal_preorder_recurrence(myself->right);
+            Travesal_preorder_recurrence_helper(root->left);
+            Travesal_preorder_recurrence_helper(root->right);
         }
+        return;
+    }
+    template<typename T>
+    void BinTree<T>::Travesal_preorder_recurrence(){
+        this->Travesal_preorder_recurrence_helper(this);
+        return;
     }
 
     //中序遍历递归，以所给参数为根节点开始遍历
     template<typename T>
-    void BinTree<T>::Travesal_inorder_recurrence (BinTree<T> *myself){
-        if(myself){
-            Travesal_preorder_recurrence(myself->left);
+    void BinTree<T>::Travesal_inorder_recurrence_helper (BinTree<T> *root){
+        if(root){
+            Travesal_inorder_recurrence_helper(root->left);
             //在此进行你要执行的操作
-            cout<<myself->val<<endl;
+            cout<<root->val<<endl;
 
 
 
 
             //在此以上进行你要执行的操作
-            Travesal_preorder_recurrence(myself->right);
+            Travesal_inorder_recurrence_helper(root->right);
         }
+    }
+    template<typename T>
+    void BinTree<T>::Travesal_inorder_recurrence(){
+        this->Travesal_inorder_recurrence_helper(this);
     }
 
     //后序遍历递归，以所给参数为根节点开始遍历
     template<typename T>
-    void BinTree<T>::Travesal_postorder_recurrence (BinTree<T> *myself){
-        if(myself){
-            Travesal_preorder_recurrence(myself->left);
-            Travesal_preorder_recurrence(myself->right);
+    void BinTree<T>::Travesal_postorder_recurrence_helper (BinTree<T> *root){
+        if(root){
+            Travesal_postorder_recurrence_helper(root->left);
+            Travesal_postorder_recurrence_helper(root->right);
             //在此进行你要执行的操作
-            cout<<myself->val<<endl;
+            cout<<root->val<<endl;
 
 
 
@@ -299,105 +306,161 @@ BinTree<T>::BinTree(vector<T> preorder, vector<T> inorder) {
             //在此以上进行你要执行的操作
         }
     }
+    template<typename T>
+    void BinTree<T>::Travesal_postorder_recurrence(){
+        this->Travesal_postorder_recurrence_helper(this);
+    }
+
+    //给定一个二叉树的 根节点 this，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+    template<typename T>
+    vector<T> BinTree<T>::rightSideView(){
+        vector<T>ret;
+        queue<BinTree<T>*> Q;
+        Q.push(this);
+        while (!Q.empty()){
+            int x=Q.size();
+            for(int i=0;i<x;i++){
+                BinTree<T>* node=Q.front();Q.pop();
+                if(i==x-1)ret.push_back(node->val);
+                if(node->left){Q.push(node->left);}
+                if(node->right){Q.push(node->right);}
+            }
+        }
+        return ret;
+    }
 
     //得到树的高度，以所给参数为根节点开始计算高度
     template<typename T>
-    int BinTree<T>::GetHeight (BinTree<T> *myself){
+    int BinTree<T>::GetHeight_helper(BinTree<T> *root){
         int heifht_left,height_right,height;
-        if(myself){
-            heifht_left=GetHeight(myself->left);
-            height_right=GetHeight(myself->right);
+        if(root){
+            heifht_left=GetHeight_helper(root->left);
+            height_right=GetHeight_helper(root->right);
             height= max(heifht_left,height_right);
             return (height+1);
         } else{
             return 0;
         }
     }
+    template<typename T>
+    int BinTree<T>::GetHeight(){
+        return this->GetHeight_helper(this);
+    }
 
     //检验对称二叉树
     template<typename T>
     inline bool BinTree<T>::IsSymmetric(){
-        return check(this, this);
+        return IsSymmetric_helper(this, this);
     }
     template<typename T>
-    bool BinTree<T>::check(BinTree<T>root1,BinTree<T>root2){
+    bool BinTree<T>::IsSymmetric_helper(BinTree<T>root1,BinTree<T>root2){
         BinTree<T>* p= this;
         BinTree<T>* q= this;
         if (!p && !q) return true;
         if (!p || !q) return false;
-        return p->val == q->val && check(p->left, q->right) && check(p->right, q->left);
+        return p->val == q->val && IsSymmetric_helper(p->left, q->right) && IsSymmetric_helper(p->right, q->left);
     }
 
 
     //翻转二叉树
     template<typename T>
-    BinTree<T>* BinTree<T>::InvertTree(BinTree<T> myself){
-        if (myself == nullptr) {
+    BinTree<T>* BinTree<T>::InvertTree(BinTree<T> root){
+        if (root == nullptr) {
             return nullptr;
         }
-        BinTree<T>* bt1 = invertTree(myself->left);
-        BinTree<T>* bt2 = invertTree(myself->right);
-        myself->left = bt2;
-        myself->right = bt1;
-        return myself;
+        BinTree<T>* bt1 = invertTree(root->left);
+        BinTree<T>* bt2 = invertTree(root->right);
+        root->left = bt2;
+        root->right = bt1;
+        return root;
 
     }
 
     //给你二叉树的根节点root 和一个表示目标和的整数targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和targetSum 。如果存在，返回 true ；否则，返回 false 。
     template<typename T>
-    bool BinTree<T>::hasPathSum(BinTree<T>* myself, int targetSum) {
-        if(!myself)return false;
-        if(!myself->left&&!myself->right)return targetSum==myself->val;
-        return hasPathSum(myself->left,targetSum-myself->val) || hasPathSum(myself->right,targetSum-myself->val);
+    bool BinTree<T>::hasPathSum_helper(BinTree<T>* root, T targetSum) {
+        if(!root)return false;
+        if(!root->left && !root->right)return targetSum==root->val;
+        return hasPathSum_helper(root->left,targetSum-root->val) || hasPathSum_helper(root->right,targetSum-root->val);
+    }
+    template<typename T>
+    bool BinTree<T>::hasPathSum(T targetSum) {
+        return hasPathSum_helper(this,targetSum);
+    }
+
+    //给你二叉树的根节点 this 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+    template<typename T>
+    void BinTree<T>::pathSum_helper(BinTree<T>*root,T targetSum,vector<vector<T>>&ret,vector<T>&path){
+        if (root == nullptr) {
+            return;
+        }
+        path.emplace_back(root->val);
+        targetSum -= root->val;
+        if (root->left == nullptr && root->right == nullptr && targetSum == 0) {
+            ret.emplace_back(path);
+        }
+        pathSum_helper(root->left, targetSum,ret,path);
+        pathSum_helper(root->right, targetSum,ret,path);
+        path.pop_back();
+    }
+    template<typename T>
+    vector<vector<T>> BinTree<T>::pathSum(T targetSum){
+        vector<vector<T>>ret;vector<T>path;
+        this->pathSum_helper(this,targetSum,ret,path);
+        return ret;
     }
 
     //给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
     template<typename T>
-    bool BinTree<T>::inline_IsValidBST(BinTree<T>* root, long long lower, long long upper) {
+    bool BinTree<T>::IsValidBST_helper(BinTree<T>* root, long long lower, long long upper) {
         if (root == nullptr) {
             return true;
         }
         if (root -> val <= lower || root -> val >= upper) {
             return false;
         }
-        return inline_IsValidBST(root -> left, lower, root -> val) && inline_IsValidBST(root -> right, root -> val, upper);
+        return IsValidBST_helper(root -> left, lower, root -> val) && IsValidBST_helper(root -> right, root -> val, upper);
     }
     template<typename T>
     bool BinTree<T>::IsValidBST() {
-        return inline_IsValidBST(this, LONG_MIN, LONG_MAX);
+        return IsValidBST_helper(this, LONG_MIN, LONG_MAX);
     }
 
     //基于二叉搜索树的查找操作，返回值为所找到的元素的节点，若找不到则返回NULL，以所给参数为根节点开始查找
     template<typename T>
-    BinTree<T>* BinTree<T>::Find_recurrence(T x,BinTree<T> *bt){
-        if(!bt)return NULL;
-        if(x>bt->val){
-            return Find_recurrence(x,bt->right);
-        } else if(x<bt->val){
-            return Find_recurrence(x,bt->left);
-        }else{
-            return bt;
+    BinTree<T>* BinTree<T>::Find_recurrence_helper(T x,BinTree<T> *root){
+        if(!root)return NULL;
+        if(x>root->val){
+            return Find_recurrence_helper(x,root->right);
+        } else if(x<root->val){
+            return Find_recurrence_helper(x,root->left);
+        }else if(x==root->val){
+            return root;
         }
     }
     template<typename T>
-    BinTree<T>* BinTree<T>::Find_commone(T x,BinTree<T> * bt){
-        if(!bt)return NULL;
-        while (bt){
-            if(x<bt->val){
-                bt=bt->left;
-            }else if(x>bt->val){
-                bt=bt->right;
-            }else{
-                return bt;
+    BinTree<T>* BinTree<T>::Find_recurrence(T x){
+        return this->Find_recurrence_helper(x,this);
+    }
+    template<typename T>
+    BinTree<T>* BinTree<T>::Find_common(T x){
+        BinTree<T>* temp= this;
+        while (temp){
+            if(x<temp->val){
+                temp=temp->left;
+            }else if(x>temp->val){
+                temp=temp->right;
+            }else if(x==temp->val){
+                return temp;
             }
         }
     }
 
-    //给定一个二叉搜索树 root 和一个目标结果 k，如果二叉搜索树中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+    //给定一个二叉搜索树 this 和一个目标结果 k，如果二叉搜索树中存在两个元素且它们的和等于给定的目标结果，则返回 true。
     template<typename T>
-    bool BinTree<T>::findTarget(BinTree<T>* root, int k) {
+    bool BinTree<T>::findTarget(T k) {
         BinTree<T> *temp;unordered_set<int>S;
-        temp=root;
+        temp=this;
         queue<BinTree<T>*> Q;
         if(!temp)return false;
         Q.push(temp);
@@ -432,79 +495,101 @@ BinTree<T>::BinTree(vector<T> preorder, vector<T> inorder) {
         return ancestor;
     }
 
-    //基于二叉搜索树找最大值，若找到则返回其最大值的结点，若不存在则返回NULL,以所给参数为根结点进行查找
+    //基于二叉搜索树找最大值，若找到则返回其最大值的结点，若不存在则返回NULL,以根结点进行查找
     template<typename T>
-    BinTree<T>* BinTree<T>::FindMax_recurrence(BinTree<T>* bt){
-        if(!bt)return NULL;
-        else if(!bt->right)return bt;
-        else if(bt->right)return FindMax_recurrence(bt->right);
+    BinTree<T>* BinTree<T>::FindMax_recurrence_helper(BinTree<T>* root){
+        if(!root)return NULL;
+        else if(!root->right)return root;
+        else if(root->right)return FindMax_recurrence_helper(root->right);
     }
     template<typename T>
-    BinTree<T>* BinTree<T>::FindMax_common(BinTree<T>* bt){
-        if(bt){
-            while (bt->right)bt=bt->right;
-            return bt;
-        }
-        else if(bt==NULL){
-            return  NULL;
-        }
-    }
-
-    //基于二叉搜索树找最小值，若找到则返回其最大值的结点，若不存在则返回NULL,以所给参数为根结点进行查找
-    template<typename T>
-    BinTree<T>* BinTree<T>::FindMin_recurrence(BinTree<T>* bt){
-        if(!bt)return NULL;
-        else if(!bt->left)return bt;
-        else if(bt->left)return FindMin_recurrence(bt->left);
+    BinTree<T>* BinTree<T>::FindMax_recurrence(){
+        return this->FindMax_recurrence_helper(this);
     }
     template<typename T>
-    BinTree<T>* BinTree<T>::FindMin_common(BinTree<T>* bt){
-        if(bt){
-            while (bt->left)bt=bt->left;
-            return bt;
+    BinTree<T>* BinTree<T>::FindMax_common(){
+        BinTree<T>*temp=this;
+        if(temp){
+            while (temp->right)temp=temp->right;
         }
-        else if(bt==NULL){
-            return  NULL;
-        }
+        return temp;
     }
 
-    //基于二叉搜索树插入某个元素，需要给定二叉搜索树的根结点和所插入元素，返回值也是二叉搜索树的根结点,若插入的元素已经存在，返回NULL
+    //基于二叉搜索树找最小值，若找到则返回其最大值的结点，若不存在则返回NULL,以根结点进行查找
     template<typename T>
-    BinTree<T>* BinTree<T>::insert(T x,BinTree<T>* myself){
-        if(!myself){
-            myself =new BinTree<T>;
-            myself->val=x;
+    BinTree<T>* BinTree<T>::FindMin_recurrence_helper(BinTree<T>* root){
+        if(!root)return NULL;
+        else if(!root->left)return root;
+        else if(root->left)return FindMin_recurrence_helper(root->left);
+    }
+    template<typename T>
+    BinTree<T>* BinTree<T>::FindMin_recurrence(){
+        return this->FindMin_recurrence_helper(this);
+    }
+    template<typename T>
+    BinTree<T>* BinTree<T>::FindMin_common(){
+        BinTree<T>*temp=this;
+        if(temp){
+            while (temp->left)temp=temp->left;
+        }
+        return temp;
+    }
+
+    //基于二叉搜索树插入某个元素，需要给定所插入元素，返回值是插入后二叉搜索树的根结点,若插入的元素已经存在，返回NULL
+    template<typename T>
+    BinTree<T>* BinTree<T>::insert_helper(T x,BinTree<T>* root){
+        if(!root){
+            root =new BinTree<T>;
+            root->val=x;
         } else{
-            if(x<myself->val){
-                myself->left= insert(x,myself->left);
-            } else if(x>myself->val){
-                myself->right= insert(x,myself->right);
-            } else if(x==myself->val){
+            if(x<root->val){
+                root->left= insert_helper(x,root->left);
+            } else if(x>root->val){
+                root->right= insert_helper(x,root->right);
+            } else if(x==root->val){
                 return NULL;
             }
         }
-        return myself;
+        return root;
+    }
+    template<typename T>
+    BinTree<T>* BinTree<T>::insert(T x){
+        return this->insert_helper(x,this);
     }
 
-    //基于二叉搜索树删除某个元素，需要给定二叉搜索树的根结点和所删除元素，返回值也是二叉搜索树的根结点
+    //基于二叉搜索树删除某个元素，需要所删除元素，返回值是删除后二叉搜索树的根结点
     template<typename T>
-    BinTree<T>* BinTree<T>::delete_BinTree (T x,BinTree<T>* bt){
-        if(!bt)cout<<"The element you want to delete doesn't exist!"<<endl;
-        else if(x<bt->val){
-            bt->left= delete_BinTree(x,bt->left);
-        }else if(x>bt->val){
-            bt->right= delete_BinTree(x,bt->right);
-        }else if(x==bt->val){
-            if(bt->left&&bt->right){
-                BinTree<T> *temp= FindMin_common(bt->right);
-                bt->val=temp->val;
-                bt->right= delete_BinTree(bt->val,bt->right);
+    BinTree<T>* BinTree<T>::delete_bt_helper ( T x, BinTree<T>* root ){
+        if( !root )cout<<"The element you want to delete doesn't exist!"<<endl;
+        else if( x < root->val ){
+            root->left = delete_bt_helper(x,root->left);
+        }else if( x > root->val ){
+            root->right = delete_bt_helper(x,root->right);
+        }else if( x == root->val ){
+            if( root->left && root->right ){
+                BinTree<T> * temp = root->right->FindMin_common();
+                root->val = temp->val;
+                root->right = delete_bt_helper(temp->val,root->right);
             }else{
-                if(!bt->left)bt=bt->right;
-                else if(!bt->right)bt=bt->left;
+                if( !root->left ){
+                    BinTree<T> * temp=root->right;
+                    root->val=temp->val;
+                    root->left=temp->left;
+                    root->right=temp->right;
+                }
+                else if( !root->right ){
+                    BinTree<T> * temp=root->left;
+                    root->val=temp->val;
+                    root->left=temp->left;
+                    root->right=temp->right;
+                }
             }
         }
-        return bt;
+        return root;
+    }
+    template<typename T>
+    BinTree<T>* BinTree<T>::delete_bt(T x){
+        return this->delete_bt_helper(x,this);
     }
 
 
--- lib/Tree.h
+++ lib/Tree.h
@@ -53,67 +53,102 @@ public:
     void Travesal_levelorder();
 
     //按层形成二维数组（层序遍历的应用）
-    vector<vector<int>> levelOrder() ;
+    vector<vector<T>> levelOrder() ;
 
-    //给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
-    vector<vector<int>> zigzagLevelOrder();
+    //给你二叉树的根节点 this ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+    vector<vector<T>> zigzagLevelOrder();
 
     //先序遍历递归，以所给参数为根节点开始遍历
-    void Travesal_preorder_recurrence (BinTree<T> *myself);
+private:
+    void Travesal_preorder_recurrence_helper (BinTree<T> *root);
+public:
+    void Travesal_preorder_recurrence ();
 
     //中序遍历递归，以所给参数为根节点开始遍历
-    void Travesal_inorder_recurrence (BinTree<T> *myself);
+private:
+    void Travesal_inorder_recurrence_helper (BinTree<T> *root);
+public:
+    void Travesal_inorder_recurrence ();
 
     //后序遍历递归，以所给参数为根节点开始遍历
-    void Travesal_postorder_recurrence (BinTree<T> *myself);
+private:
+    void Travesal_postorder_recurrence_helper (BinTree<T> *root);
+public:
+    void Travesal_postorder_recurrence ();
+
+    //给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+    vector<T> rightSideView();
 
     //得到树的高度，以所给参数为根节点开始计算高度
-    int GetHeight (BinTree<T> *myself);
+private:
+    int GetHeight_helper (BinTree<T> *root);
+public:
+    int GetHeight();
 
     //检验对称二叉树
 public:
     inline bool IsSymmetric();
 private:
-    bool check(BinTree<T>root1,BinTree<T>root2);
+    bool IsSymmetric_helper(BinTree<T>root1,BinTree<T>root2);
 
 public:
     //翻转二叉树
-    BinTree<T>* InvertTree(BinTree<T> myself);
+    BinTree<T>* InvertTree(BinTree<T> root);
 
-    //给你二叉树的根节点root 和一个表示目标和的整数targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和targetSum 。如果存在，返回 true ；否则，返回 false 。
-    bool hasPathSum(BinTree<T>* myself, int targetSum) ;
-
-    //给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+    //给你二叉树的根节点this 和一个表示目标和的数targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和targetSum 。如果存在，返回 true ；否则，返回 false 。
 private:
-    bool inline_IsValidBST(BinTree<T>* root, long long lower, long long upper) ;
+    bool hasPathSum_helper(BinTree<T>* root, T targetSum);
+public:
+    bool hasPathSum(T targetSum);
+
+    //给你二叉树的根节点 this 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+private:
+    void pathSum_helper(BinTree<T>*root,T targetSum,vector<vector<T>>&ret,vector<T>&path);
+public:
+    vector<vector<T>> pathSum(T targetSum);
+
+    //给你一个二叉树的根节点 this ，判断其是否是一个有效的二叉搜索树。
+private:
+    bool IsValidBST_helper(BinTree<T>* root, long long lower, long long upper) ;
 public:
     bool IsValidBST() ;
 
-public:
     //基于二叉搜索树的查找操作，返回值为所找到的元素的节点，若找不到则返回NULL，以所给参数为根节点开始查找
-    BinTree<T>* Find_recurrence(T x,BinTree<T> *bt);
-    BinTree<T>* Find_commone(T x,BinTree<T> * bt);
+private:
+    BinTree<T>* Find_recurrence_helper(T x,BinTree<T> *root);
+public:
+    BinTree<T>* Find_recurrence(T x);
+public:
+    BinTree<T>* Find_common(T x);
 
-    //给定一个二叉搜索树 root 和一个目标结果 k，如果二叉搜索树中存在两个元素且它们的和等于给定的目标结果，则返回 true。
-    bool findTarget(BinTree<T>* root, int k) ;
+    //给定一个二叉搜索树 this 和一个目标结果 k，如果二叉搜索树中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+    bool findTarget(T k) ;
 
     //给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
     //百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
     BinTree<T>* lowestCommonAncestor(BinTree<T>* p, BinTree<T>* q) ;
 
     //基于二叉搜索树找最大值，若找到则返回其最大值的结点，若不存在则返回NULL,以所给参数为根结点进行查找
-    BinTree<T>* FindMax_recurrence(BinTree<T>* bt);
-    BinTree<T>* FindMax_common(BinTree<T>* bt);
+    BinTree<T>* FindMax_recurrence_helper(BinTree<T>* root);
+    BinTree<T>* FindMax_recurrence();
+    BinTree<T>* FindMax_common();
 
     //基于二叉搜索树找最小值，若找到则返回其最大值的结点，若不存在则返回NULL,以所给参数为根结点进行查找
-    BinTree<T>* FindMin_recurrence(BinTree<T>* bt);
-    BinTree<T>* FindMin_common(BinTree<T>* bt);
+    BinTree<T>* FindMin_recurrence_helper(BinTree<T>* root);
+    BinTree<T>* FindMin_recurrence();
+    BinTree<T>* FindMin_common();
 
-    //基于二叉搜索树插入某个元素，需要给定二叉搜索树的根结点和所插入元素，返回值也是二叉搜索树的根结点,若插入的元素已经存在，返回NULL
-    BinTree<T>* insert(T x,BinTree<T>* myself);
+    //基于二叉搜索树插入某个元素，需要给定所插入元素，返回值是插入后二叉搜索树的根结点,若插入的元素已经存在，返回NULL
+private:
+    BinTree<T>* insert_helper(T x,BinTree<T>* root);
+public:
+    BinTree<T>* insert(T x);
 
-    //基于二叉搜索树删除某个元素，需要给定二叉搜索树的根结点和所删除元素，返回值也是二叉搜索树的根结点
-    BinTree<T>* delete_BinTree (T x,BinTree<T>* bt);
+    //基于二叉搜索树删除某个元素，需要给定所删除元素，返回值是删除后二叉搜索树的根结点,若删除的元素不存在，返回NULL
+private:
+    BinTree<T>* delete_bt_helper (T x,BinTree<T>* root);
+public:
+    BinTree<T>* delete_bt(T x);
 };
 
 #endif
