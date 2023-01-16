af60e33f73a4620e7010e792cdffbf6c39a45641
XuHan <736946693@qq.com>
2023-01-16 23:47:01 +0800

    20230116 23:46

--- lib/DataStructure.h
+++ lib/DataStructure.h
@@ -2,18 +2,11 @@
 #define  __myalgorithm_h__
 
 
-#include "authority.h"
-#include "leetcode.h"
 #include "leetcode.cpp"
-#include "Heap.h"
 #include "Heap.cpp"
-#include "Tree.h"
 #include "Tree.cpp"
-#include "Graph.h"
 #include "Graph.cpp"
-#include "SetType.h"
 #include "SetType.cpp"
-#include "Sort.h"
 #include "Sort.cpp"
 
 
